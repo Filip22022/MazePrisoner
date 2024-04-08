474b6ad318567d0a19636ab54107eb46b816df0c
Filip <filipgolyszny@gmail.com>
2024-04-08 19:45:31 +0200

    fix room positions

---
+++ scenes/rooms/start_room/start_room.gd
@@ -0,0 +1,30 @@
+extends Scene
+
+func initialize(entered_from: Directions.Direction, connections: Dictionary):
+	super.initialize(entered_from, connections)
+	self.open_doors(connections) 
+
+func open_doors(directions):
+	for direction in directions.keys():
+		var door = get_door(direction)
+		door.open()
+		
+func close_doors(directions):
+	for direction in directions.keys():
+		var door = get_door(direction)
+		door.close()
+		
+func get_door(direction: Directions.Direction):
+	match direction:
+		Directions.Direction.Up:
+			if self.has_node("DoorUp"):
+				return self.get_node("DoorUp")
+		Directions.Direction.Right:
+			if self.has_node("DoorRight"):
+				return self.get_node("DoorRight")
+		Directions.Direction.Down:
+			if self.has_node("DoorDown"):
+				return self.get_node("DoorDown")
+		Directions.Direction.Left:
+			if self.has_node("DoorLeft"):
+				return self.get_node("DoorLeft")
