7ceeb4ebfa91a9b78c1dd9efae3f8ca6c86323b4
Filip <filipgolyszny@gmail.com>
2024-04-27 12:32:01 +0200

    delete old files, add base enemy script

--- characters/enemies/enemy.gd
+++ characters/enemies/enemy.gd
@@ -1,14 +1,13 @@
-extends Node2D
+class_name Enemy
+extends Node
 
 var health: float
-
-func _init():
-	self.health = 100
 
 func take_damage(damage: float):
 	self.health -= damage
 	if health <= 0.0:
 		die()
 		
+
 func die():
 	queue_free()
---
+++ characters/enemies/enemy1/enemy1.gd
@@ -0,0 +1,4 @@
+extends Enemy
+
+func _init():
+	self.health = 100.0
