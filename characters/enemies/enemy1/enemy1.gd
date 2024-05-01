92ed6527a3d5844647b0ca19b5afd0e1de5e6628
Filip Gołyszny <filipgolyszny@gmail.com>
2024-05-01 17:50:11 +0200

    make the enemy follow the player

--- characters/enemies/enemy1/enemy1.gd
+++ characters/enemies/enemy1/enemy1.gd
@@ -1,4 +1,12 @@
 extends Enemy
 
+var speed = 100
+
 func _init():
 	self.health = 100.0
+
+func _process(delta):
+	var player = PlayerInfo.get_player()
+	if player:
+		self.velocity = (player.get_global_position() - self.position).normalized() * speed
+		move_and_slide()
