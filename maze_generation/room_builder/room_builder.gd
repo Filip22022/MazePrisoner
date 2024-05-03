34a9b227f685dac7d5951bfcf8de4e73d1974698
Filip Gołyszny <filipgolyszny@gmail.com>
2024-05-03 22:05:28 +0200

    Add possible walls

--- maze_generation/room_builder/room_builder.gd
+++ maze_generation/room_builder/room_builder.gd
@@ -1,16 +1,23 @@
 class_name RoomBuilder
 extends Node2D
 
-var room_height = 160
-var room_width = 320
+var room_height = 320
+var room_width = 480
+var room
 
 func _init():
 	build()
 
 func build():
-	var room = load("res://maze_generation/room_builder/empty_room.tscn").instantiate()
+	room = load("res://maze_generation/room_builder/empty_room.tscn").instantiate()
 	add_child(room)
 	
+	build_walls()
+	
+	var player = load("res://player/player.tscn").instantiate()
+	add_child(player)
+	
+func build_walls():
 	var left_wall = load("res://maze_generation/room_builder/walls/left_wall_open.tscn").instantiate()
 	left_wall.global_position = Vector2i(-room_width/2,0)
 	room.add_child(left_wall)
@@ -26,6 +33,3 @@ func build():
 	var bottom_wall = load("res://maze_generation/room_builder/walls/bottom_wall_closed.tscn").instantiate()
 	bottom_wall.global_position = Vector2i(0, room_height/2)
 	room.add_child(bottom_wall)
-	
-	var player = load("res://player/player.tscn").instantiate()
-	add_child(player)
